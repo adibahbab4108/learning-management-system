64565193b06a3062018dd5bbb3827cf346063842
adibahbab4108 <adibalvi476@gmail.com>
2025-03-25 03:54:18 +0600

    feat: toggle system update

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -4,10 +4,12 @@ import appContext from "../../context/AppContext";
 import Loading from "../../components/student/Loading";
 import { assets } from "../../assets/assets";
 import { FaUser } from "react-icons/fa";
+import humanizeDuration from "humanize-duration";
 
 const CourseDetails = () => {
     const { id } = useParams()
     const [courseData, setCourseData] = useState(null)
+    const [openSections, setOpenSections] = useState({})
     const { allCourses, calculateRating, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures } = useContext(appContext)
     const rating = Math.floor(calculateRating(courseData));
 
@@ -16,6 +18,15 @@ const CourseDetails = () => {
         setCourseData(findCourse || null)
 
     }, [allCourses, id])
+
+    const toggleSection = (index) => {
+        setOpenSections((prev) => (
+            {
+                ...prev, [index]: !prev[index]
+            }
+        ))
+    }
+
 
     return courseData ? (
         <>
@@ -42,12 +53,30 @@ const CourseDetails = () => {
                             {
                                 courseData.courseContent.map((chapter, index) => (
                                     <div key={index} className="border border-gray-300 bg-white mb-2 rounded">
-                                        <div className="flex items-center justify-between px-4 py-3 cursor-pointer select-none">
+                                        <div onClick={() => toggleSection(index)} className="flex items-center justify-between px-4 py-3 cursor-pointer select-none">
                                             <div className="flex items-center gap-2">
                                                 <img src={assets.down_arrow_icon} alt="arrow icon" />
                                                 <p className="font-medium md:text-base text-sm">{chapter.chapterTitle}</p>
                                             </div>
                                             <p className="text-sm ">{chapter.chapterContent.length} lectures - {calculateChapterTime(chapter)}</p>
+                                        </div>
+                                        <div className={`overflow-hidden transition-all duration-300 ${openSections[index] ? 'max-h-96' : 'max-h-0'} `}>
+                                            <ul className="list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300">
+                                                {
+                                                    chapter.chapterContent.map((lecture, index) => (
+                                                        <li key={index} className="flex items-start gap-2 py-1">
+                                                            <img src={assets.play_icon} alt="play icon" className="w-4 h-4 mt-1" />
+                                                            <div className="flex items-center justify-between w-full text-gray-800 text-xs md:text-base">
+                                                                <p>{lecture.lectureTitle}</p>
+                                                                <div className="flex gap-2">
+                                                                    {lecture.isPreviewFree && <p className="text-blue-500 cursor-pointer">Preview</p>}
+                                                                    <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, { units: ['h', 'm'] })}</p>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    ))
+                                                }
+                                            </ul>
                                         </div>
                                     </div>
                                 ))
