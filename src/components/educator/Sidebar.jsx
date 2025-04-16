b0badd946052cb3d38f887fe2ba8e76445281658
adibahbab4108 <adibalvi476@gmail.com>
2025-04-17 00:20:13 +0600

    updated sidebar

--- src/components/educator/Sidebar.jsx
+++ src/components/educator/Sidebar.jsx
@@ -12,7 +12,6 @@ const Sidebar = () => {
         { name: "Add Course", path: '/educator/add-course', icon: assets.add_icon },
         { name: "My Courses", path: '/educator/my-courses', icon: assets.my_course_icon },
         { name: "Student Enrolled", path: '/educator/student-enrolled', icon: assets.person_tick_icon },
-
     ]
     const handleSideBar = () => {
         setShowSideBar(!showSideBar)
@@ -40,7 +39,7 @@ const Sidebar = () => {
                         key={item.name}
                         end={item.path === '/educator'}
                         className={({ isActive }) =>
-                            `flex items-center gap-3 ${showSideBar ? 'px-4':'px-2'}  py-3.5 whitespace-nowrap transition-all duration-200
+                            `flex items-center gap-3 ${showSideBar ? 'px-4' : 'px-2'}  py-3.5 whitespace-nowrap transition-all duration-200
                  ${isActive
                                 ? 'bg-indigo-100 border-r-4 border-indigo-500 font-medium text-indigo-700'
                                 : 'hover:bg-gray-100 border-r-4 border-transparent hover:border-gray-300'}`
