6f05809b8f6feda22d8db9e7957572fd9b909cbb
adibahbab4108 <adibalvi476@gmail.com>
2025-04-24 18:28:08 +0600

    User Profile page created

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,8 +1,27 @@
+import { useContext } from "react";
+import AuthContext from "../context/AuthContext";
+import Loading from "../components/student/Loading";
 
 const UserProfile = () => {
+    const { user } = useContext(AuthContext)
+    if (!user) return <Loading />
     return (
         <div>
-            UserProfile
+            <div className="relative">
+                {/* Cover photo section */}
+                <div className="border cover h-52 flex items-center justify-center bg-blue-300" >
+                    <p>Cover photo</p>
+                </div>
+                {/* Profile picture section */}
+                <div className="flex flex-col justify-center items-center top-40  absolute  left-1/2  transform -translate-x-1/2">
+                    <div className="border">
+                        <img src={user.photoURL} className="w-36 h-36 bg-green-300" alt="Profile" />
+                    </div>
+                    <h2 className="text-xl font-semibold my-3">
+                        {user.displayName}
+                    </h2>
+                </div>
+            </div>
         </div>
     );
 };
